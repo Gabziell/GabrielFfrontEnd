8a489263a26c6c3dbe948c02f71647c32dbe9a7c
Gabriel <142261891+Gabziell@users.noreply.github.com>
2023-09-18 16:57:07 -0300

    Add files via upload

---
+++ senhordosgames/SENHORDOSGAMES.js
@@ -0,0 +1,11 @@
+const meuBotao = document.getElementById("meuBotao")
+const meuBotao2 = document.getElementById("meuBotao2")
+const meuBotao3 = document.getElementById("meuBotao3")
+function redirecionar() {
+
+ window.location.href = "https://uudt-ma.github.io/img/regra/regra5.jpg"
+}
+
+meuBotao.addEventListener("click", redirecionar)
+meuBotao2.addEventListener("click", redirecionar)
+meuBotao3.addEventListener("click", redirecionar)
